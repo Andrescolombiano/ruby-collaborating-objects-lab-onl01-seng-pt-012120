df626b067ca79afc0737765a918aaa0ff5fe12d6
Andres Carvajal <jc1993box@hotmail.com>
2020-03-04 08:19:46 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,21 +1,12 @@
 class MP3Importer
 
-    attr_accessor :path, :filename
+attr_accessor :path
 
-    @@files = []
+def initialize(path)
+  @path=path
+end
+  
 
-    def initialize(path = @path)
-     @path = path
-   end
-
-    def files
-     files = Dir.entries("#{path}").select {|f| !File.directory? f}
-     files.each do |item|
-       @@files << item
-     end
-   end
-
-    def import
-     @@files.each{ |filename| Song.new_by_filename(filename) }
-   end
+def files
+  self.files.length {song
  end 
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
 class Song 
 
-    attr_accessor :name, :artist, :songs 
+    attr_accessor :name, :artist
 
     @@all = []
 
-    def initialize(name = @name)
+    def initialize(name)
      @name = name
      @@all << self
    end
@@ -12,16 +12,4 @@ class Song
     def self.all
      @@all
    end
-
-    def artist_name=(name)
-     guy = Artist.find_or_create_by_name(name)
-     guy.add_song(self)
-     guy
-   end
-
-    def self.new_by_filename(filename)
-     song = self.new(filename.split(" - ")[1])
-     song.artist_name = filename.split(" - ")[0]
-     song
-   end
- end
+end
