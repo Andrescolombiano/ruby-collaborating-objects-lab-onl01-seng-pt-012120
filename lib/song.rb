3bc346088faf4fef70c04473bc4a4a132f6ef5df
Andres Carvajal <jc1993box@hotmail.com>
2020-03-04 15:22:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,4 +12,10 @@ class Song
     def self.all
      @@all
    end
+   
+   def self.new_by_filename(name)
+     split_name = name.split (" - ")
+     song = self.new(split_name[1])
+     song.artist = split_name[0]
+   end 
 end
